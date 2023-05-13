3eab5788619ab0097f6f7684d65b25628ca39f32
carlosetellolee <107780940+carlosetellolee@users.noreply.github.com>
2023-05-12 22:48:00 -0500

    Experiencia 1. Mediateca
    Experiencia 1. Mediateca INAH (en limpio)

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,4 +50,65 @@ function incorrecta3()
         document.getElementById("msg7").innerHTML = ans7;
     }
 
+function incorrecta4()
+    {
+        var ans8 = document.getElementById("q4_ans1").name;
+        document.getElementById("msg8").innerHTML = ans8;
+    }
+
+function correcta3()
+    {
+        var ans9 = document.getElementById("q4_ans2").name;
+        document.getElementById("msg9").innerHTML = ans9;
+    }
+
+function incorrecta5()
+    {
+        var ans10 = document.getElementById("q5_ans1").name;
+        document.getElementById("msg10").innerHTML = ans10;
+    }
+
+function correcta4()
+    {
+        var ans11 = document.getElementById("q5_ans2").name;
+        document.getElementById("msg11").innerHTML = ans11;
+    }
+
+function incorrecta6()
+    {
+        var ans12 = document.getElementById("q6_ans1").name;
+        document.getElementById("msg12").innerHTML = ans12;
+    }
+
+function incorrecta7()
+    {
+        var ans13 = document.getElementById("q6_ans2").name;
+        document.getElementById("msg13").innerHTML = ans13;
+    }
+
+function correcta5()
+    {
+        var ans14 = document.getElementById("q6_ans3").name;
+        document.getElementById("msg14").innerHTML = ans14;
+    }
+
+function incorrecta8()
+    {
+        var ans15 = document.getElementById("q7_ans1").name;
+        document.getElementById("msg15").innerHTML = ans15;
+    }
+
+function correcta6()
+    {
+        var ans16 = document.getElementById("q7_ans2").name;
+        document.getElementById("msg16").innerHTML = ans16;
+    }
+
+function incorrecta9()
+    {
+        var ans17 = document.getElementById("q7_ans3").name;
+        document.getElementById("msg17").innerHTML = ans17;
+    }
+
+
 var map = L.map('map',{ center: [40.418889, -3.691944], zoom: 6, maxBounds: [[39.53,-13.09],[40,6.24]], minZoom: 5, maxZoom: 7 }); L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '© <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
